4c163ca0b3e38f3086956187e852515a268f2d6c
Jayakrishnan G <jkdesignlab@gmail.com>
2018-06-23 19:40:32 +0530

    Added association info to migrations

--- db/migrate/20180619021931_create_orders.rb
+++ db/migrate/20180619021931_create_orders.rb
@@ -1,6 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
+      t.references :user
       t.float :order_amount
       t.date :order_date
       t.integer :order_tracking_number
--- db/migrate/20180619022413_create_sub_categories.rb
+++ db/migrate/20180619022413_create_sub_categories.rb
@@ -1,6 +1,7 @@
 class CreateSubCategories < ActiveRecord::Migration[5.2]
   def change
     create_table :sub_categories do |t|
+      t.references :category
       t.string :name
       t.text :description
 
